331fa7c293f9c950ea4c94a6bb72668dd8023b76
spencerwencer <spencerwise@spencers-mbp-3.home>
2014-10-04 13:02:45 -0600

    FInished assessment... remove isn't working

---
+++ js/mainCtrl.js
@@ -0,0 +1,25 @@
+var app = angular.module('todo');
+
+app.controller('mainCtrl', function($scope, todoService){
+
+
+
+	$scope.getData = function(){
+		$scope.items = todoService.getData();
+	};
+
+	$scope.postItem = function(){
+		var newItem = {};
+		newItem.text = $scope.newText;
+		todoService.postItem(newItem);
+		$scope.newText = '';
+
+	};
+
+	$scope.removeItem = function(itemToRemove){
+		todoService.removeItem(itemToRemove);
+	};
+
+	$scope.getData();
+
+});
---
+++ js/todoService.js
@@ -0,0 +1,29 @@
+var app = angular.module('todo');
+
+app.service('todoService', function(){
+
+	var items = [];
+
+	this.getData = function(){
+
+		return items;
+	};
+
+	this.postItem = function(newItem){
+
+		return items.push(newItem);
+	};
+
+	this.removeItem = function(item){
+
+		for(var i = 0; i < items.length; i++){
+			if(items[i].text === item){
+				items.splice(i, 1);
+				return;
+			};
+
+	};
+
+};
+
+});
